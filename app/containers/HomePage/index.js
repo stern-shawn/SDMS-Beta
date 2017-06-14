9cbb0149132091454aab5a2ee76fc2db258eb033
Shawn Stern <stern.shawn@gmail.com>
2017-06-13 23:56:45 -0700

    Successful experiment with glamorous for CSS-in-JS + Bulma
    -Creating a Bulma hero section with glamorous components for semantic component names instead of 'div soup' for the sake of layout

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,8 +7,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import glamorous from 'glamorous';
 
 // Components
+
+// Styles
+import bulma from 'styles/bulma.scss';
+
+// Experiment with using glamorous to create semantically named components
+// and see if it plays nicely with Bulma framework!
+const Hero = glamorous.section(
+  bulma.hero,
+  bulma['is-primary'],
+  (props) => (bulmaMapping[props.size])
+);
+
+const HeroBody = glamorous.div(
+  bulma['hero-body'],
+);
+
+const Container = glamorous.div(
+  bulma.container,
+);
+
+const Title = glamorous.h1(
+  bulma.title,
+);
+
+const SubTitle = glamorous.h2(
+  bulma.subtitle,
+);
+
+// Create an object for easy mapping of JS-firendly prop names to bulma's hypenated classes
+const bulmaMapping = {
+  isFullheight: bulma['is-fullheight'],
+};
 
 
 const HomePage = ({ routeParams }) => (
@@ -22,9 +55,18 @@ const HomePage = ({ routeParams }) => (
         },
       ]}
     />
-    <h1>
-      Home
-    </h1>
+    <Hero size="isFullheight">
+      <HeroBody>
+        <Container>
+          <Title>
+            Welcome
+          </Title>
+          <SubTitle>
+            Strength Development Management System
+          </SubTitle>
+        </Container>
+      </HeroBody>
+    </Hero>
   </article>
 );
 
