aa26b29d1f1efffb32f156257dd4ff29db175764
Shawn Stern <stern.shawn@gmail.com>
2017-06-21 23:24:44 -0700

    Removing unused CSSTransitionGroup import from Dashboard

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
