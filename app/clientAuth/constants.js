ffcee2d71245012fe42d9ac1dd24b513973531ef
Shawn Stern <stern.shawn@gmail.com>
2017-07-30 09:18:25 -0700

    Adding inProgress flag and alphabetically ordering constants/reducer cases
    - Flag will be helpful for triggering animations while flows such as SIGN_IN -> async request to server -> AUTH_USER/AUTH_ERROR are in progress and not yet determined
    - Aphabets are neat for ordering stuff

--- app/clientAuth/constants.js
+++ app/clientAuth/constants.js
@@ -2,8 +2,8 @@
  * Action Constants
  */
 
+export const AUTH_ERROR = 'sdms/auth/AUTH_ERROR';
 export const AUTH_USER = 'sdms/auth/AUTH_USER';
 export const DEAUTH_USER = 'sdms/auth/DEAUTH_USER';
-export const AUTH_ERROR = 'sdms/auth/AUTH_ERROR';
 export const SIGN_IN = 'sdms/auth/SIGN_IN';
 export const SIGN_OUT = 'sdms/auth/SIGN_OUT';
--- app/clientAuth/reducer.js
+++ app/clientAuth/reducer.js
@@ -4,38 +4,53 @@
 import { fromJS } from 'immutable';
 
 import {
+  AUTH_ERROR,
   AUTH_USER,
   DEAUTH_USER,
-  AUTH_ERROR,
+  SIGN_IN,
+  SIGN_OUT,
 } from './constants';
 
 const initialState = fromJS({
   authenticated: false,
-  token: null,
-  userProfile: null,
   errors: null,
+  inProgress: false,
+  token: null, // Token is cached in localStorage, this is here so we don't have to do an IO operation with localStorage for EVERY request
+  userProfile: null,
 });
 
 function authReducer(state = initialState, action) {
   switch (action.type) {
+    case AUTH_ERROR:
+      console.log('Error in sign-in/up process');
+      return state
+        .set('errors', action.payload.error);
     case AUTH_USER:
       console.log('User authenticated');
       console.log(action.payload);
       return state
         .set('authenticated', true)
         .set('errors', null)
+        .set('inProgress', false)
         .set('token', action.payload.token)
         .set('userProfile', action.payload.userProfile);
     case DEAUTH_USER:
-      console.log('Logging user out');
+      console.log('User logged out');
       return state
         .set('authenticated', false)
+        .set('inProgress', false)
         .set('token', null)
         .set('userProfile', null);
-    case AUTH_ERROR:
-      console.log('Error in sign-in/up process');
+    case SIGN_IN:
+      console.log('User submitting credentials to sign in');
       return state
-        .set('errors', action.payload.error);
+        .set('errors', null)
+        .set('inProgress', true);
+    case SIGN_OUT:
+      console.log('User requesting sign out');
+      return state
+        .set('errors', null)
+        .set('inProgress', true);
     default:
       return state;
   }
