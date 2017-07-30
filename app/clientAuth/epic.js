43261c6ad0c9ce515af5cb225d7c4e6ef1f74f4e
Shawn Stern <stern.shawn@gmail.com>
2017-07-30 11:45:31 -0700

    Adding localStorage saves and react-router redirect on successful signing
    - Caching token and profile into localStorage so it can be retrieved in later sessions if user leaves site
    - Successful login should redirect to a user dashboard, profile page, or such. Adding stub code for later as a reminder

--- app/clientAuth/epic.js
+++ app/clientAuth/epic.js
@@ -1,5 +1,6 @@
 import { ajax } from 'rxjs/observable/dom/ajax';
 import { Observable } from 'rxjs';
+import { browserHistory } from 'react-router';
 import {
   AUTH_ERROR,
   AUTH_USER,
@@ -20,7 +21,15 @@ const signInEpic = (action$) =>
         action.payload, // SIGN_IN payload object resolves to { email, password } to fit endpoint
         { 'Content-Type': 'application/json' } // Need to explicitly define that we're sending JSON
       )
-        .map((res) => signInSuccess(res.response))
+        .map(({ response }) => {
+          // Cache the JWT and userProfile for later logins, etc
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('userProfile', JSON.stringify(response.userProfile));
+          // Redirect to success page or something to be defined later
+          browserHistory.push('/TBD');
+          // Dispatch action to save data to store
+          return signInSuccess(response);
+        })
         .catch((err) => Observable.of(signInError(err)))
     );
 
