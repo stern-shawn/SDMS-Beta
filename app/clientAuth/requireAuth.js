5b73d579accaf0f7b994e4a1504def7c3251ebe3
Shawn Stern <stern.shawn@gmail.com>
2017-07-30 12:29:40 -0700

    Adding requireAuth HOC to prevent protected components/routes from rendering

---
+++ app/clientAuth/requireAuth.js
@@ -0,0 +1,42 @@
+// HOC for protecting sensitive routes from unauthenticated users
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
+import { createStructuredSelector } from 'reselect';
+
+import { makeSelectAuthenticated } from 'clientAuth/selectors';
+
+export default function (ComposedComponent) {
+  class Authentication extends Component {
+    componentWillMount() {
+      // Redirect user to home if they are not authenticated
+      if (!this.props.authenticated) {
+        browserHistory.push('/');
+      }
+    }
+
+    // Add in an update hook so that if the user signs out (which will update the authentication prop)
+    // we run a check and do a forced redirect to home
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        browserHistory.push('/');
+      }
+    }
+
+    // Render the passed component, and pass down any addition properties that we've added using ES6 spread
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  }
+
+  Authentication.propTypes = {
+    authenticated: PropTypes.bool.isRequired,
+  };
+
+  const mapStateToProps = createStructuredSelector({
+    authenticated: makeSelectAuthenticated(),
+  });
+
+  return connect(mapStateToProps)(Authentication);
+}
